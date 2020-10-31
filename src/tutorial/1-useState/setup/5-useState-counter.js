8eddfbef3aff8e591edf3550ccbba77cc53c9656
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-10-31 15:18:17 -0400

    Set up counter functionality; implement passing in function as argument to setValue method.

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,33 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+	const [value, setValue] = useState(0)
+	const increase = () => {
+		setValue(value + 1)
+	}
+
+	const complexIncrease = () => {
+		// setTimeout(() => setValue(value + 1), 2000)
+		setTimeout(() => setValue((prevState) => prevState + 1), 2000)
+	}
+	return (
+		<>
+			<section style={{ margin: '4rem 0' }}>
+				<h2>Regular Counter</h2>
+				<h1>{value}</h1>
+				<button className="btn" onClick={() => setValue(value - 1)}>Decrease</button>
+				<button className="btn" onClick={() => setValue(0)}>Reset</button>
+				<button className="btn" onClick={increase}>Increase</button>
+			</section>
+			<section>
+				<h2>More Complex Counter</h2>
+				<h1>{value}</h1>
+				<button className="btn" onClick={complexIncrease}>Complex Increase</button>
+				<button className="btn" onClick={() => setValue(0)}>Reset</button>
+				<button className="btn" onClick={increase}>Increase</button>
+			</section>
+		</>
+	);
 };
 
 export default UseStateCounter;
