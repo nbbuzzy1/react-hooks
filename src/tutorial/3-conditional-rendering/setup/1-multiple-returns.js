77bc9eff4e10c0726aa22afdea0a8a2458bea437
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-11-01 08:30:32 -0500

    Conditional display in react.

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,50 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+	const [loading, setLoading] = useState(true);
+	const [isError, setError] = useState(false);
+	const [user, setUser] = useState('default user');
+
+	useEffect(() => {
+		fetch(url)
+			.then((resp) => {
+				if (resp.status >= 200 && resp.status <= 299) {
+					return resp.json();
+				}
+
+				setLoading(false);
+				setError(true);
+				throw new Error(resp.statusText)
+			})
+			.then(({ login }) => {
+				setUser(login)
+				setLoading(false);
+			})
+			.catch((error) => console.log(error))
+	}, [])
+
+
+	if (loading) {
+		return (
+			<div>
+				<h2>Loading...</h2>
+			</div>
+		)
+	}
+
+	if (isError) {
+		return (
+			<div>
+				<h2>Error...</h2>
+			</div>
+		)
+	}
+
+	return (
+		<div>
+			<h1>{user}</h1>
+		</div>
+	);
 };
 
 export default MultipleReturns;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,21 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
-
-  return <h2>short circuit</h2>;
+	const [text, setText] = useState('nick');
+	const [isError, setIsError] = useState(false);
+	// const firstValue = text || 'hello world';
+	// const secondValue = text && 'hello world';
+	const clickHandler = () => setIsError(!isError);
+	return (
+		<>
+			<h1>{text || 'hello world'}</h1>
+			<button className="btn" onClick={clickHandler}>Toggle Error</button>
+			{isError && <h1>Error...</h1>}
+			{isError ? <p>there is an error</p> : <div>
+				<h2>There is no error</h2>
+			</div>}
+		</>
+	)
 };
 
 export default ShortCircuit;
